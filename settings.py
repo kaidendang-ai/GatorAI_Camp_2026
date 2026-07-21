b562f963d74a5e11e995368f78e220ee9c326778
Kaiden Dang <kaiden.dang@gmail.com>
2026-07-21 14:34:23 -0400

    sans fight didn't work, so it had to go kaput

--- settings.py
+++ settings.py
@@ -182,7 +182,6 @@ NPC_DATA = {
             "Hey, you.",
             "Do you want to play poker?"
             "If you don't, we're going to have a bad time..."
-              
-        ]  # Default starter dialogue lines
+        ]
     }
 }
